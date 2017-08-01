ae4a7540c8049a64cefa0792876fb8264504ffc5
VanyAraujo <araujo.g.vanina@gmail.com>
2017-08-01 14:40:52 -0300

    NO TOCAR->paquete estructuraArbol Nario debe ser generico,mirar en la estructura de grafo sino no va andar

--- src/main/java/tpfinal/tp/estructuraArbolNario/ArbolNario.java
+++ src/main/java/tpfinal/tp/estructuraArbolNario/ArbolNario.java
@@ -12,54 +12,64 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 
-
-public class ArbolNario {
-
-    private Nodo raiz;
-    private LinkedList lista; 
-    private LinkedHashSet listaNodo;
-   
+public class ArbolNario<T> {
+ 
+    private Nodo<T> raiz;
+     
+    /**
+     * Default ctor.
+     */
     public ArbolNario() {
         super();
     }
  
-    public Nodo getTaiz() {
+    /**
+     * Return the root Node of the tree.
+     * @return the root element.
+     */
+    public Nodo<T> getRaizE() {
         return this.raiz;
     }
-
-    public void setRaiz(Nodo root) {
-        this.raiz = root;
+ 
+    /**
+     * Set the root Element for the tree.
+     * @param rootElement the root element to set.
+     */
+    public void setRaiz(Nodo<T> raiz) {
+        this.raiz = raiz;
     }
      
-    private void walk(Nodo element, List<Nodo> list) {
+    /**
+     * Returns the Tree<T> as a List of Node<T> objects. The elements of the
+     * List are generated from a pre-order traversal of the tree.
+     * @return a List<Node<T>>.
+     */
+    public List<Nodo<T>> toList() {
+        List<Nodo<T>> list = new ArrayList<Nodo<T>>();
+        walk(raiz, list);
+        return list;
+    }
+     
+    /**
+     * Returns a String representation of the Tree. The elements are generated
+     * from a pre-order traversal of the Tree.
+     * @return the String representation of the Tree.
+     */
+    public String toString() {
+        return toList().toString();
+    }
+     
+    /**
+     * Walks the Tree in pre-order style. This is a recursive method, and is
+     * called from the toList() method with the root element as the first
+     * argument. It appends to the second argument, which is passed by reference     * as it recurses down the tree.
+     * @param element the starting element.
+     * @param list the output of the walk.
+     */
+    private void walk(Nodo<T> element, List<Nodo<T>> list) {
         list.add(element);
-        for (Nodo valor : element.getHijos()) {
-            walk(valor, list);
+        for (Nodo<T> data : element.getChildren()) {
+            walk(data, list);
         }
-    }
-     @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 17 * hash + Objects.hashCode(this.raiz);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final ArbolNario other = (ArbolNario) obj;
-        if (!Objects.equals(this.raiz, other.raiz)) {
-            return false;
-        }
-        return true;
     }
 }
-
--- src/main/java/tpfinal/tp/estructuraArbolNario/Nodo.java
+++ src/main/java/tpfinal/tp/estructuraArbolNario/Nodo.java
@@ -8,82 +8,119 @@ package tpfinal.tp.estructuraArbolNario;
 import java.util.ArrayList;
 import java.util.List;
 
-
-public class Nodo {
-    public String valor;
-    public List<Nodo> hijos;
-    public TipoNodo tipo;
+public class Nodo<T> {
  
-    
+    public T data;
+    public List<Nodo<T>> children;
+ 
+    /**
+     * Default ctor.
+     */
     public Nodo() {
         super();
     }
-    
- public Nodo(String valor, TipoNodo tipo) {
-        this.tipo=tipo;
-        this.valor=valor;
-    }
  
-   
-    public Nodo(String valor) {
+    /**
+     * Convenience ctor to create a Node<T> with an instance of T.
+     * @param data an instance of T.
+     */
+    public Nodo(T data) {
         this();
-        setValor(valor);
+        setData(data);
     }
      
-    
-    public List<Nodo> getHijos() {
-        if (this.hijos == null) {
-            return new ArrayList<Nodo>();
+    /**
+     * Return the children of Node<T>. The Tree<T> is represented by a single
+     * root Node<T> whose children are represented by a List<Node<T>>. Each of
+     * these Node<T> elements in the List can have children. The getChildren()
+     * method will return the children of a Node<T>.
+     * @return the children of Node<T>
+     */
+    public List<Nodo<T>> getChildren() {
+        if (this.children == null) {
+            return new ArrayList<Nodo<T>>();
         }
-        return this.hijos;
+        return this.children;
     }
  
- 
-    public void setHijos(List<Nodo> hijos) {
-        this.hijos = hijos;
+    /**
+     * Sets the children of a Node<T> object. See docs for getChildren() for
+     * more information.
+     * @param children the List<Node<T>> to set.
+     */
+    public void setChildren(List<Nodo<T>> children) {
+        this.children = children;
     }
  
-    public int getCantidadHijos() {
-        if (hijos == null) {
+    /**
+     * Returns the number of immediate children of this Node<T>.
+     * @return the number of immediate children.
+     */
+    public int getNumberOfChildren() {
+        if (children == null) {
             return 0;
         }
-        return hijos.size();
+        return children.size();
     }
      
-    public void addHijo(Nodo nuevoHijo) {
-        if (hijos == null) {
-            hijos = new ArrayList<Nodo>();
+    /**
+     * Adds a child to the list of children for this Node<T>. The addition of
+     * the first child will create a new List<Node<T>>.
+     * @param child a Node<T> object to set.
+     */
+    public void addHijo(Nodo<T> child) {
+        if (children == null) {
+            children = new ArrayList<Nodo<T>>();
         }
-        hijos.add(nuevoHijo);
+        children.add(child);
     }
-    
-    public void inserHijoAt(int index, Nodo nuevoHijo) throws IndexOutOfBoundsException {
-        if (index == getCantidadHijos()) {
+     
+    /**
+     * Inserts a Node<T> at the specified position in the child list. Will     * throw an ArrayIndexOutOfBoundsException if the index does not exist.
+     * @param index the position to insert at.
+     * @param child the Node<T> object to insert.
+     * @throws IndexOutOfBoundsException if thrown.
+     */
+    public void insertChildAt(int index, Nodo<T> child) throws IndexOutOfBoundsException {
+        if (index == getNumberOfChildren()) {
             // this is really an append
-            addHijo(nuevoHijo);
+            addHijo(child);
             return;
         } else {
-            hijos.get(index); //just to throw the exception, and stop here
-            hijos.add(index, nuevoHijo);
+            children.get(index); //just to throw the exception, and stop here
+            children.add(index, child);
         }
     }
-    
-    public void eliminarHijo(int index) throws IndexOutOfBoundsException {
-        hijos.remove(index);
+     
+    /**
+     * Remove the Node<T> element at index index of the List<Node<T>>.
+     * @param index the index of the element to delete.
+     * @throws IndexOutOfBoundsException if thrown.
+     */
+    public void removeChildAt(int index) throws IndexOutOfBoundsException {
+        children.remove(index);
     }
  
-    public String getValor() {
-        return this.valor;
+    public T getData() {
+        return this.data;
     }
  
-    public void setValor(String valor) {
-        this.valor = valor;
+    public void setData(T data) {
+        this.data = data;
     }
      
     public String toString() {
-
-        return  "valor=" + this.valor + " tipo= "+ this.tipo; 
+        StringBuilder sb = new StringBuilder();
+        sb.append("{").append(getData().toString()).append(",[");
+        int i = 0;
+        for (Nodo<T> e : getChildren()) {
+            if (i > 0) {
+                sb.append(",");
+            }
+            sb.append(e.getData().toString());
+            i++;
+        }
+        sb.append("]").append("}");
+        return sb.toString();
     }
 }
-    
-
